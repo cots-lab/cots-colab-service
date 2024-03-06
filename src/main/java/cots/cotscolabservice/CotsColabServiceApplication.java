2851e97ca71a3a447a5cf133b2ef824eea1ec409
WKDKavishka <w.k.d.kavishka@outlook.com>
2024-03-06 20:29:30 +0530

    Initiate Project

---
+++ src/main/java/cots/cotscolabservice/CotsColabServiceApplication.java
@@ -0,0 +1,12 @@
+package cots.cotscolabservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotsColabServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CotsColabServiceApplication.class, args);
+  }
+}
---
+++ src/test/java/cots/cotscolabservice/CotsColabServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cots.cotscolabservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CotsColabServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
